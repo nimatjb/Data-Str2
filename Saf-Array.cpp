ff859f842e61cc3d5740c027687ed3f48e9b5e66
nimatjb <nima.tjb12@gmail.com>
2023-11-16 06:02:18 +0330

    commit saf-nima

---
+++ Saf-Array.cpp
@@ -0,0 +1,24 @@
+﻿void ReverseQueue() {
+    if (IsEmpty()) {
+        return;
+    }
+
+    int* reversedArr = new int[cap];
+    int index = 0;
+
+    // از انتهای صف به ابتدا آرایه‌ی برعکس را پر می‌کنیم
+    for (int i = rear; i >= front; i--) {
+        reversedArr[index++] = arr[i];
+    }
+
+    // جایگزین کردن آرایه‌ی اصلی با آرایه‌ی برعکس شده
+    delete[] arr;
+    arr = reversedArr;
+
+    // به روزرسانی ایندکس‌ها
+    front = 0;
+    rear = size - 1;
+}
+
+
+};
