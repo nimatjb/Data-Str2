46ce7813ab4ab4d435cbd6f79e865167ddd9bab2
nimatjb <nima.tjb12@gmail.com>
2023-12-23 19:54:47 +0330

    commit change

--- pwr.cpp
+++ pwr.cpp
@@ -8,14 +8,3 @@ int power(int x, int y) {
 		return x * power(x, y - 1);
 	}
 }
-int main() {
-	int a, b;
-	int num;
-	cout << " enter your number: ";
-	cin >> a;
-	cout << "enter your pow: ";
-	cin >> b;
-	num = power(a, b);
-	cout << "your answer is: " << num << endl;
-	return 0;
-}
