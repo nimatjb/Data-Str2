3bd7b255596be601947065c38ca2a84ec0112597
mrjanshiri <lmarjanshiril@gmail.com>
2024-01-04 12:48:43 -0800

    commit by marjan

---
+++ Selection Sort (1).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
